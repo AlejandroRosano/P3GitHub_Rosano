36fbf4fd1d329e01793ca57622cb41d0f06845da
RoSaNo <160237252+AlejandroRosano@users.noreply.github.com>
2024-03-08 11:07:06 +0100

    Create Aplicacion.java
    Desarrollos

---
+++ Aplicacion.java
@@ -0,0 +1,27 @@
+public class UsoWrapper {
+
+
+    public static void main(String[] args) {
+        short corto =25;
+        Short objetoShort =corto;
+        String cadena = objetoShort.toString();
+        //hace los mismo que String cadena = "25";
+        Boolean miboleano =true;
+        miboleano.booleanValue();
+            System.out.println("VERDADERO");
+        int minumero = 8;
+        Integer numeroObjeto = Integer.valueOf(minumero);
+        // hace los mismo que Integer Numeroobjeto =8;
+        System.out.println("numeroObjeto es un objeto de la clase " + numeroObjeto.getClass().getName());
+        int nuevonumero = numeroObjeto;
+        System.out.println(nuevonumero);
+        Character micaracter='a';
+        char ch = micaracter.charValue();
+        System.out.println("el valor del objeto micaracter es "+micaracter.charValue());
+        Double numdoble =2000.0;
+        numdoble= numeroObjeto.doubleValue();
+        System.out.println(numdoble);
+        
+    }
+    
+}
